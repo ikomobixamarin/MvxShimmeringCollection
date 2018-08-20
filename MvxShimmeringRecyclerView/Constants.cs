fc30d1c95a1a142b6ecb99e4835a1a3293b87d58
Adrien Padol <apadol@ikomobi.com>
2018-08-20 10:09:53 +0200

    Updated constant value

--- MvxShimmeringRecyclerView/Constants.cs
+++ MvxShimmeringRecyclerView/Constants.cs
@@ -3,7 +3,7 @@ namespace MvxShimmering
 {
     public class Constants
     {
-        public const int ShimmerPlaceholderViewType = 0;
+        public const int ShimmerPlaceholderViewType = -999999;
         public const int ShimmerAnimationDuration = 1000;
     }
 }
